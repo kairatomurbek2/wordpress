a2096886a9375bef9ce69012211f88657b753d36
kayrat <kayrat@test.com>
2015-01-15 16:19:42 +0600

    верстка внутри товарв по высоте

--- themes/bootstrap-basic/single-second_hand_products.php
+++ themes/bootstrap-basic/single-second_hand_products.php
@@ -15,7 +15,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 <?php get_sidebar('left'); ?>
 
 <div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
-    <main id="main" class="site-main" role="main">
+
         <?php
         while (have_posts()) {
             the_post();
@@ -32,7 +32,7 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 
         } //endwhile;
         ?>
-    </main>
+
 </div>
 <?php get_sidebar('right'); ?>
 <?php get_footer(); ?>
